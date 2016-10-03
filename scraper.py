f7229c0f00e5e4ebe89dc2acc39fa2fde61464ae
pattyvader <pattyloren@gmail.com>
2016-10-03 10:27:29 -0300

    Class refactored

--- scraper.py
+++ scraper.py
@@ -4,32 +4,47 @@
 from bs4 import BeautifulSoup
 
 def extract_data(html):
-    data = {}
+    url = extract_url(html)
+    title = extract_title(html)
+    description = extract_description(html)
+    image = extract_image(html)
+    text = extract_text(html)
 
+    return url, title, description, image, text
+
+def extract_url(html):
     soup = BeautifulSoup(html)
-
-    title = soup.find_all('meta', attrs={'property':'og:title'})
-
-    for i in title:
-        title = i["content"]
-
     url = soup.find_all('meta', attrs={'property':'og:url'})
 
     for i in url:
-        url = i["content"]
+        return i["content"]
 
+def extract_title(html):
+    soup = BeautifulSoup(html)
+    title = soup.find_all('meta', attrs={'property':'og:title'})
+
+    for i in title:
+        return i["content"]
+
+def extract_description(html):
+    soup = BeautifulSoup(html)
     description = soup.find_all('meta', attrs={'property':'og:description'})
 
     for i in description:
-        description = i["content"]
+        return i["content"]
 
+def extract_image(html):
+    soup = BeautifulSoup(html)
     image = soup.find_all('meta', attrs={'property':'og:image'})
 
     for i in image:
-        image = i["content"]
+        return i["content"]
 
+def extract_text(html):
     text = ""
+    soup = BeautifulSoup(html)
+
     for i in soup.find_all('p'):
         text += i.get_text()
 
-    return url, title, description, image, text
+    return text
