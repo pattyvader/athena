0320ce9cf3af31aee997e091e7547d3cbefda655
pattyvader <pattyloren@gmail.com>
2016-10-04 21:51:04 -0300

    Creates function to search for some term in the Elasticsearch.

--- web/view_athena/views.py
+++ web/view_athena/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from elasticsearch import Elasticsearch
 
 def search(request):
     if request.method == 'GET':
         term = request.GET.get('term_search')
 
-        print term
+        search_term(term)
 
         return render(request, 'view_athena/index.html', {})
+
+def search_term(term):
+    es = Elasticsearch()
+
+    res = es.search(index="athena", body={"query": {"match": {"url": "\"" + str(term) + "\""}}})
+
+    for hit in res['hits']['hits']:
+        print("%(url)s:" % hit["_source"])
