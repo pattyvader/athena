3ab7e2014f230aef6d6a7dacec7703911d4bd73a
pattyvader <pattyloren@gmail.com>
2016-10-06 23:04:51 -0300

    Add response of the Elasticsearch with pages' data.

--- web/view_athena/views.py
+++ web/view_athena/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from elasticsearch import Elasticsearch
+from django.http import HttpResponse
 
 def search(request):
     if request.method == 'GET':
         term = request.GET.get('term_search')
+        response = search_term(term)
+        pages = []
 
-        search_term(term)
+        for hit in response['hits']['hits']:
+            pages.append(hit["_source"])
 
-        return render(request, 'view_athena/index.html', {})
+        return render(request, 'view_athena/index.html', {'pages':pages})
 
 def search_term(term):
     es = Elasticsearch()
 
     res = es.search(index="athena", body={"query": {"match": {"url": "\"" + str(term) + "\""}}})
 
-    for hit in res['hits']['hits']:
-        print("%(url)s:" % hit["_source"])
+    return res
